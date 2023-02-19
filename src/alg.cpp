94e48bc6a1c18a6e723704c04bee5bc0edf577aa
Tatsu <68650923+TreadTinker@users.noreply.github.com>
2023-02-19 16:17:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+     if (a % b == 0)
+        return b;
+    if (b % a == 0)
+        return a;
+    if (a > b)
+        return greatest_common_divisor(a%b, b);
+    return greatest_common_divisor(a, b%a);
 }
